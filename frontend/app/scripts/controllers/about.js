8dfec388f272cca5fcd15faea875aaf861e78b16
rattrr <rii.ratte@gmail.com>
2016-08-05 12:23:07 +0200

    moze nie popsuje

---
+++ frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name praktykiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the praktykiApp
+ */
+angular.module('praktykiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name praktykiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the praktykiApp
+ */
+angular.module('praktykiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
