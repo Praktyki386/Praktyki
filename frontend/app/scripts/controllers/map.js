18fcafc0684d89c2f06b98d4085aa531e7d818ba
domikam <dominika.mikolajka@gmail.com>
2016-08-08 11:32:43 +0200

    Add files via upload
    osobny plik na mapę, jeden marker

---
+++ frontend/app/scripts/controllers/map.js
@@ -0,0 +1,17 @@
+var map;
+      function initMap() {
+        var mapOptions = {
+                  zoom: 14,
+                  center: new google.maps.LatLng(51.25,22.54),
+                  mapTypeId: google.maps.MapTypeId.TERRAIN
+              }
+
+        map = new google.maps.Map(document.getElementById('map'), mapOptions);
+          
+        var marker=new google.maps.Marker({
+          position: new google.maps.LatLng(51.25,22.54)
+          });
+
+        marker.setMap(map);
+      }
+
