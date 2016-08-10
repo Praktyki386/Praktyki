81373fd52a5a5535dd803385d735321bbda82b4e
domikam <dominika.mikolajka@gmail.com>
2016-08-10 12:24:43 +0200

    syntax fixed

--- frontend/app/scripts/controllers/map.js
+++ frontend/app/scripts/controllers/map.js
@@ -1,10 +1,11 @@
+'use strict';
 var map;
       function initMap() {
         var mapOptions = {
                   zoom: 14,
                   center: new google.maps.LatLng(51.25,22.54),
                   mapTypeId: google.maps.MapTypeId.TERRAIN
-              }
+              };
 
         map = new google.maps.Map(document.getElementById('map'), mapOptions);
           
