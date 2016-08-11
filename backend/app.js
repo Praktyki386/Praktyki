b1818c929b7c23254a50b7816c4a43c2cee91c73
rattrr <rii.ratte@gmail.com>
2016-08-11 15:38:01 +0200

    app is now returning json with user data

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,70 @@
 var express = require('express');
+var session = require('express-session');
 var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Is it working?');
-});
-
-app.listen(3000, function () {
-  console.log('Express app listening on port 3000!');
-});
-
-
-
-
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: '573206371038-nctb938nlkjqtqf1ou30ord5grc01r8h.apps.googleusercontent.com',
-    clientSecret: 'V9HjVx1GmB6OheN1EfJ7s8Fe',
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      console.log(profile);
-      return done(null, profile);
-  }
+	clientID: '573206371038-nctb938nlkjqtqf1ou30ord5grc01r8h.apps.googleusercontent.com',
+	clientSecret: 'V9HjVx1GmB6OheN1EfJ7s8Fe',
+	callbackURL: "http://localhost:3000/auth/google/callback"
+}, function(accessToken, refreshToken, profile, done) {
+	return done(null, {
+		id: profile.id,
+		username: profile.displayName,
+		photo: profile.photos.length ? profile.photos[0].value : null
+	});
+}
 ));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
 passport.serializeUser(function(user, done) {
-  done(null, user);
+	done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-  done(null, user);
+	done(null, user);
 });
 
+app.use(session({
+	secret: 'secret'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 app.get('/auth/google',
-  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+	passport.authenticate('google', {
+		scope: ['https://www.googleapis.com/auth/plus.login']
+	}));
 
 app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+	passport.authenticate('google', {
+		failureRedirect: '/login'
+	}), function(req, res) {
+		res.redirect('/');
+	});
+
+
+app.get('/', function(req, res) {
+	var session = req.session;
+	var username = 'unknown user, please <a href="/auth/google">login</a>';
+	if (session.passport && session.passport.user && session.passport.user.username) {
+		username = session.passport.user.username;
+	}
+	res.send('Hello ' + username + '. ' + '<a href="/user">Click here for json</a>');
+});
+
+app.get('/user', function(req, res) {
+	var session = req.session;
+	var user = null;
+	if (session.passport && session.passport.user) {
+		user = session.passport.user;
+	}
+	res.send({
+		user: user
+	});
+});
+
+
+app.listen(3000);
