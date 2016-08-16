761e3947b81dcde2c917dd4e13e431b8856642c9
domikam <dominika.mikolajka@gmail.com>
2016-08-16 14:08:36 +0200

    Log in redirect to google form

--- frontend/app/scripts/controllers/map.js
+++ frontend/app/scripts/controllers/map.js
@@ -2,7 +2,7 @@
 
 
 angular.module("praktykiApp", ['uiGmapgoogle-maps'])
-        .controller("Example", function($scope) {
+        .controller("mapCtrl", function($scope) {
                 $scope.map = {
                     zoom: 14,
                     center: new google.maps.LatLng(51.25,22.54),
